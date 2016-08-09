297bd8d8138e1d2379ba9e8ad1233b04413d6274
Palm <waraporn.v@augmentis.biz>
2016-08-09 16:55:04 +0700

    edit fragment

--- app/src/main/java/ayp/aug/contactapp/ContactFragment.java
+++ app/src/main/java/ayp/aug/contactapp/ContactFragment.java
@@ -5,10 +5,15 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.util.List;
 
@@ -16,6 +21,13 @@ import java.util.List;
  * Created by Waraporn on 8/9/2016.
  */
 public class ContactFragment extends Fragment {
+
+    private EditText name;
+    private EditText tel;
+    private EditText email;
+    private ImageView photoView;
+    private ImageButton photoButton;
+    private Button button_delete;
 
     public static ContactFragment newInstance() {
         
@@ -26,61 +38,78 @@ public class ContactFragment extends Fragment {
         return fragment;
     }
 
-    Contact contact;
-    ContactLab contactLab;
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        contact = new Contact();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_contact_list,container,false);
 
-        RecyclerView recycleView = (RecyclerView) v.findViewById(R.id.fragment_contact_recycleview);
-        recycleView.setLayoutManager(new GridLayoutManager(getActivity(),3));// 3 is columns
-//        recycleView.setAdapter(new ContactAdapter(contactLab.getContacts()));
+        View v = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        name = (EditText) v.findViewById(R.id.contact_name);
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        tel = (EditText) v.findViewById(R.id.contact_tel);
+        tel.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        email = (EditText) v.findViewById(R.id.contact_email);
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        photoView = (ImageView) v.findViewById(R.id.contact_frame_photo);
+
+        photoButton = (ImageButton) v.findViewById(R.id.contact_camera);
+
+        button_delete = (Button) v.findViewById(R.id.button_delete);
 
         return v;
-    }
-
-
-    private class ContactHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        ContactHolder(LayoutInflater layoutInflater, ViewGroup viewGroup){
-            super(layoutInflater.inflate(R.layout.list_item_contact, viewGroup, false));
-        }
-
-        @Override
-        public void onClick(View v) {
-
-        }
-    }
-
-    private class ContactAdapter extends RecyclerView.Adapter<ContactHolder> {
-
-        private List<ContactLab> contactList;
-
-        ContactAdapter(List<ContactLab> contacts) {
-            contactList = contacts;
-        }
-
-        @Override
-        public ContactHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
-        }
-
-        @Override
-        public void onBindViewHolder(ContactHolder holder, int position) {
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return 1;
-        }
     }
 
 }
