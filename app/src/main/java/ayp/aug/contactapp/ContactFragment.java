65415b46dfcb5ddf6360695ae16d5d68ebc365c6
wilailux <lllwilailux@gmail.com>
2016-08-09 15:00:07 +0700

    edit

--- app/src/main/java/ayp/aug/contactapp/ContactFragment.java
+++ app/src/main/java/ayp/aug/contactapp/ContactFragment.java
@@ -3,6 +3,11 @@ package ayp.aug.contactapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Waraporn on 8/9/2016.
@@ -24,5 +29,17 @@ public class ContactFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         contact = new Contact();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_contact,container,false);
+
+        RecyclerView recycleView = (RecyclerView) v.findViewById(R.id.fragment_contact_recycleview);
+
+        recycleView.setLayoutManager(new GridLayoutManager(getActivity(),3));// 3 is columns
+
+        return v;
     }
 }
---
+++ app/src/main/java/ayp/aug/contactapp/database/ContactBaseHelper.java
@@ -0,0 +1,8 @@
+package ayp.aug.contactapp.database;
+
+/**
+ * Created by Wilailux on 8/9/2016.
+ */
+public class ContactBaseHelper {
+
+}
