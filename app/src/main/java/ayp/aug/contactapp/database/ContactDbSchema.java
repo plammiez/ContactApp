d5988a940e8546fc95432be707534758eaa7bc0a
Palm <waraporn.v@augmentis.biz>
2016-08-09 14:30:58 +0700

    edit fragment_contact layout

---
+++ app/src/main/java/ayp/aug/contactapp/database/ContactDbSchema.java
@@ -0,0 +1,19 @@
+package ayp.aug.contactapp.database;
+
+/**
+ * Created by Waraporn on 8/9/2016.
+ */
+public class ContactDbSchema {
+
+    public static final class ContactTable{
+
+        public static final String NAME = "contacts";
+
+        public static final class Cols{
+            public static final String NAME = "name";
+            public static final String TEL = "tel";
+            public static final String EMAIL = "email";
+        }
+
+    }
+}
